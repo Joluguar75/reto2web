df5bce55d5517859e223036818d5b7bdf7390e7c
Joluguar75 <jorge.gutierrez.mt@correo.usa.edu.co>
2021-12-01 13:42:31 -0500

    inicializar

---
+++ src/main/java/Reto2_Web/repositorio/FraganceRepositorio.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reto2_Web.repositorio;
+
+import Reto2_Web.interfaces.InterfaceFragance;
+import Reto2_Web.modelo.Fragance;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class FraganceRepositorio {
+    @Autowired
+    private InterfaceFragance repository;
+
+    public List<Fragance> getAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Fragance> getClothe(String reference) {
+        return repository.findById(reference);
+    }
+    public Fragance create(Fragance clothe) {
+        return repository.save(clothe);
+    }
+
+    public void update(Fragance clothe) {
+        repository.save(clothe);
+    }
+    
+    public void delete(Fragance clothe) {
+        repository.delete(clothe);
+    }
+}
